4c95a2061301fde537c1a1ee67fd2fc8d267cb2c
Markkus Millend <markusmillend@me.com>
2017-12-16 13:30:55 +0200

    Add MouseEvent methods

--- src/xyz/mllnd/JavaPaint.java
+++ src/xyz/mllnd/JavaPaint.java
@@ -1,6 +1,7 @@
 package xyz.mllnd;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -18,4 +19,13 @@ public class JavaPaint {
             }
         });
     }
+
+    public void mousePressed(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseDragged(MouseEvent e) {}
+    public void mouseClicked(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+    public void mouseMoved(MouseEvent e) {}
+    
 }
